235fbe9674366e43b0372325c614786006174226
Tushar <tusharpatwork@gmail.com>
2020-08-14 17:15:40 -0400

    Initial Version of the code

---
+++ src/main/java/com/demo/istiodemo/IstiodemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.istiodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IstiodemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IstiodemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/istiodemo/IstiodemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.istiodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IstiodemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
